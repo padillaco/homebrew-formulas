10daecaed8b3727e984b7ad81f83fb86a355c3eb
David Russell <david.russell@padillaco.com>
2025-07-07 18:38:06 -0400

    sha256

---
+++ Formula/pantheon-sync.rb
@@ -0,0 +1,19 @@
+
+# This formula installs the Pantheon Sync script, which allows users to sync content from Pantheon sites to their local machine.
+class PantheonSync < Formula
+  desc "Sync content from Pantheon sites to your local machine"
+  homepage "https://github.com/padillaco/homebrew-pantheon-sync"
+  url "https://github.com/padillaco/homebrew-pantheon-sync/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "564022f3f661e906968bd7557d40aee7980155933db5400c7c37d17c6ea7d626"
+  license "MIT"
+
+  depends_on "bash"
+
+  def install
+    bin.install "pantheon-sync.sh" => "pantheon-sync"
+  end
+
+  test do
+    system "#{bin}/pantheon-sync", "--version"
+  end
+end

8dfc484527c80399bcad7867a8f83b3595c4b312
David Russell <david.russell@padillaco.com>
2025-08-07 13:43:53 -0400

    v0.4.5

--- Formula/pantheon-sync.rb
+++ Formula/pantheon-sync.rb
@@ -3,8 +3,8 @@
 class PantheonSync < Formula
   desc "Sync content from Pantheon sites to your local machine"
   homepage "https://github.com/padillaco/homebrew-pantheon-sync"
-  url "https://github.com/padillaco/homebrew-pantheon-sync/archive/refs/tags/v0.4.4.tar.gz"
-  sha256 "33513f9171f877610f57ebeb305725d55606484e39df7c4c0fdb88a546a32fb0"
+  url "https://github.com/padillaco/homebrew-pantheon-sync/archive/refs/tags/v0.4.5.tar.gz"
+  sha256 "0ccfc91ebe57303d99c3baa6b1fc381da1706f4e593bf5537fe80f2a04d791a6"
   license "MIT"
 
   depends_on "bash"
